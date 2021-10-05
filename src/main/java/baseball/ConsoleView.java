fd035bb7fde240d247b475f3e06aa6c86c246abb
cobiyu <cobiyu90@gmail.com>
2021-10-06 01:14:07 +0900

    refactor(ConsoleView): 관련 메소드 static 전환

--- src/main/java/baseball/ConsoleView.java
+++ src/main/java/baseball/ConsoleView.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import baseball.domains.GameResultType;
 import baseball.domains.ResultState;
 import nextstep.utils.Console;
 
@@ -15,30 +14,30 @@ public class ConsoleView {
    * 숫자 입력
    * @return 입력한 숫자
    */
-  public String printInputNumber(){
+  public static String printInputNumber(){
     System.out.print("숫자를 입력해주세요 : ");
-    
+
     return Console.readLine();
   }
 
   /**
    * 게임 결과 출력
    */
-  public void printResult(ResultState resultState){
+  public static void printResult(ResultState resultState){
     System.out.println(resultState.getStateMessage());
   }
 
   /**
    * 게임 끝 메시지 출력
    */
-  public void printSuccessMessage(){
+  public static void printSuccessMessage(){
     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
   }
 
   /**
    * 새 게임 입력
    */
-  public boolean isContinueReGame(){
+  public static boolean isContinueReGame(){
     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     String inputStr = Console.readLine();
 
